c1e6eee468243850593d833460da4c05e05239f5
bshimel-cloudera <106165837+bshimel-cloudera@users.noreply.github.com>
2022-08-30 14:50:45 -0500

    Update add.py
    Removed tracking

--- code/python/add.py
+++ code/python/add.py
@@ -9,9 +9,5 @@ while (args >= x):
     print ("Argument %i: %s" % (x, sys.argv[x]))
     sum = sum + int(sys.argv[x])
     x = x + 1
-    
-output_file = open("result.txt", "w")
-output_file.write("Sum: " + str(sum))
-output_file.close()
 
 print ("Sum of the numbers is: %i." % sum)
