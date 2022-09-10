9da32527160c76e6abc38c7fee5d614f049a058d
Bruce Shimel <bshimel@cloudera.com>
2022-09-10 12:49:43 -0500

    updated add.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CML on CDP - Experiment Tracking
 
 This is a simple project to demonstrate experiment tracking in CML on CDP.
+
+VERSION: g2073ffe
--- code/python/add.py
+++ code/python/add.py
@@ -9,5 +9,15 @@ while (args >= x):
     print ("Argument %i: %s" % (x, sys.argv[x]))
     sum = sum + int(sys.argv[x])
     x = x + 1
+    
+with open("result.txt", "w") as output_file:
+    output_file.write(f"Input: {sys.argv[1:]},")
+    output_file.write(f"Sum: {sum}")
+    output_file.close()
+
+cdsw.track_metric("Count", args)
+cdsw.track_metric("Sum", sum)
+
+cdsw.track_file("result.txt")
 
 print ("Sum of the numbers is: %i." % sum)
