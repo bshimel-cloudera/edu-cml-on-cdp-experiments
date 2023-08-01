16a92e0ffb05bd43d0af50162be2d0bd9894a59f
Bruce Shimel <bshimel@cloudera.com>
2023-07-31 22:32:43 -0500

    Resolved conflict

--- code/python/add.py
+++ code/python/add.py
@@ -2,10 +2,5 @@ import sys
 
 params = [int(number) for number in sys.argv[1:]]
 total = sum(params)
-<<<<<<< HEAD
 
 print (f"The sum of the numbers is: {total}")
-=======
-    
-print (f"The sum of the numbers is: {total}")
->>>>>>> refs/remotes/origin/main
